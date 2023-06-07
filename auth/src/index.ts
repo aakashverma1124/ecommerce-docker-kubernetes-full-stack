5529713095d8ba6ac8f851c635b850395e045032
aakashverma <info.aakash11@gmail.com>
2023-06-08 00:15:46 +0530

    Added basic apis and error handlers

---
+++ auth/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+
+const app = express();
+
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.use(errorHandler);
+
+app.listen(3002, () => {
+    console.log('Listening on port 3002..')
+});
